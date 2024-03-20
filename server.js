d4e80832e14095198be057a76150632e6c6c0b66
Manish22112002 <161568453+Manish22112002@users.noreply.github.com>
2024-03-20 11:13:37 +0530

    Three

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express")
 const app = express()
 const connectDB = require("./database/db")
+const  routes = require("./Auth/route")
+const passwordReset = require("./Auth/passwordReset")
 
 
 connectDB();
@@ -9,7 +11,9 @@ connectDB();
 app.use(express.json());
 
 app.use("/api/auth", require("./Auth/route"))
-// app.use("/api/aut")
+
+app.use("/api/routes", routes);
+app.use("/api/passwordReset", passwordReset);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Connected to port ${PORT}`))
