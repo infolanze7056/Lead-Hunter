c725a58e75898b95a8976632057d91e7e7ccaf7c
infolanze <infolanze.tech@gmail.com>
2024-04-04 10:12:42 +0530

    First

--- backend/routes/phoneperoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const { newPayment, statusCheck } = require("../controllers/paymentphoneController.js");
-
-const router = express.Router()
-
-router.route("/payment").post(newPayment)
-router.route('/status').post(statusCheck);
-
-module.exports = router;
