97483e8955784e12bf6950aaa24ac7c2a374965b
infolanze <infolanze.tech@gmail.com>
2024-05-02 18:45:49 +0530

    Phonepe check

---
+++ src/Components/Dashboard/Remote.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import DashNav from './DashNav'
+import Free from '../Free'
+import RemoteJobs from './RemoteJob/RemoteJobs'
+
+function Remote() {
+  return (
+    <div>
+      <DashNav />
+      <Free />
+      <RemoteJobs />
+    </div>
+  )
+}
+
+export default Remote
